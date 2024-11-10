b1a87363b2697e9326eca16efb220d791f327168
jackmcosta32 <joaomcosta32@gmail.com>
2024-11-10 20:52:36 -0300

    refactor: rework tailwind configs

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,17 +1,11 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('../../tailwind-workspace-preset')],
   content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
+    join(__dirname, './src/**/*!(*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
--- libs/presentation/tailwind.config.js
+++ libs/presentation/tailwind.config.js
@@ -1,7 +1,11 @@
 const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('../../tailwind-workspace-preset')],
-  content: [join(__dirname, './src/**/*!(*.stories|*.spec).{ts,tsx,html}')],
+  content: [
+    join(__dirname, './src/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
 };
