2ce2ec535c4f929714bcee450c46ec75a5a9a86c
jackmcosta32 <joaomcosta32@gmail.com>
2024-11-11 21:55:16 -0300

    chore: adds next secure header and bundle analyzer

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,6 +2,8 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const { createSecureHeaders } = require('next-secure-headers');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -12,11 +14,27 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          // HSTS Preload: https://hstspreload.org/
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 86_400_000, includeSubDomains: true, preload: true },
+          ],
+        }),
+      },
+    ];
+  },
 };
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
