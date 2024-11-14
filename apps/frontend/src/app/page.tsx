42b339cf23dc69db13190f8385cb371fc026b683
jackmcosta32 <joaomcosta32@gmail.com>
2024-11-13 22:38:49 -0300

    refactor: removes boilerplate code on frontend

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,11 +1,3 @@
-import { SignUpForm } from '@libs/presentation/src/components/auth/sign-up-form';
-import { Button } from '@template/ui/components/button';
-
 export default function Index() {
-  return (
-    <div>
-      <SignUpForm />
-      <Button variant="destructive">click me</Button>
-    </div>
-  );
+  return <div>home</div>;
 }
